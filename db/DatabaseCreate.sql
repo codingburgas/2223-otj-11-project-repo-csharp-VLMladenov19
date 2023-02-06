a9144958cd7e035f87d5d5912aeb42f596a24917
Vasil Mladenov <VLMladenov19@codingburgas.bg>
2023-02-06 15:32:10 +0200

    Add essential folders and files

---
+++ db/DatabaseCreate.sql
@@ -0,0 +1,2 @@
+--create database OTJTraining
+USE OTJTraining
---
+++ src/2223-otj-11-project-repo-csharp-VLMladenov19/2223-otj-11-project-repo-csharp-VLMladenov19/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2223_otj_11_project_repo_csharp_VLMladenov19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
