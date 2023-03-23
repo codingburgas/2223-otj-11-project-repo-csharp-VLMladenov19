b936e516302685f7175c11667825d94989054c4d
Vasil Mladenov <VLMladenov19@codingburgas.bg>
2023-03-23 20:10:45 +0200

    Create Outfits adding menu

---
+++ src/WardrobeManager/wm.console/OutfitMenu/AddOutfitMenu.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wm.bll;
+using wm.console.ClotheMenu;
+
+namespace wm.console.OutfitMenu
+{
+    public class AddOutfitMenu
+    {
+        public static void Print(int userId)
+        {
+            Console.Clear();
+            Console.WriteLine("============   Add Outfit   ============");
+            Console.WriteLine($"{"Type [B] to go back to Main Menu",36}");
+            Console.WriteLine($"{"Date Format: YYYY-MM-DD",32}\n");
+
+            string name = InsertName(userId);
+            var date = DateTime.Today;
+
+            OutfitService.AddOutfit(name, date, userId);
+
+            Console.WriteLine($"\n{"Outfit Added",26}");
+            Console.WriteLine($"\n{"Press [A] key to Add new Outfits",36}");
+            Console.WriteLine($"{"or any other key to go back",34}");
+            Console.WriteLine($"\n========================================");
+
+            var input = Char.ToUpper(Console.ReadKey().KeyChar);
+            if (input == 'A')
+                AddOutfitMenu.Print(userId);
+            else
+                OutfitsListMenu.Print(userId);
+        }
+
+        private static string InsertName(int userId)
+        {
+            Console.Write($"{"Name: ",22}");
+            var name = Console.ReadLine();
+
+            if (name.ToUpper() == "B")
+            {
+                MainMenu.Print();
+            }
+            if (name.IsNullOrEmpty())
+            {
+                Console.WriteLine($"\n{"Name is required",28}");
+                Console.WriteLine($"\n========================================");
+                Console.ReadKey();
+                Print(userId);
+            }
+            if (OutfitService.GetOutfitId(name, userId) != -1)
+            {
+                Console.WriteLine($"\n{"Name already in use",30}");
+                Console.WriteLine($"\n========================================");
+                Console.ReadKey();
+                Print(userId);
+            }
+
+            return name;
+        }
+    }
+}
