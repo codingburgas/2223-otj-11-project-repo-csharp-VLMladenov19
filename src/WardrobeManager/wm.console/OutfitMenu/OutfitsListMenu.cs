2180d0d364fed5d0dcab733a34fdd1e2fa661fba
Vasil Mladenov <VLMladenov19@codingburgas.bg>
2023-03-23 20:10:17 +0200

    Create Outfits Menu

---
+++ src/WardrobeManager/wm.console/OutfitMenu/OutfitsListMenu.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wm.bll;
+using wm.console.ClotheMenu;
+
+namespace wm.console.OutfitMenu
+{
+    public class OutfitsListMenu
+    {
+        public static void Print(int userId)
+        {
+            Console.Clear();
+            Console.WriteLine("============  Outfits List  ============");
+            Console.WriteLine();
+
+            PrintOutfitList(userId);
+
+            Console.WriteLine($"\n{"Press [A] key to Create new Outfit",37}");
+            Console.WriteLine($"{"or any other key to go back",34}");
+            Console.WriteLine($"\n========================================");
+
+            var input = Char.ToUpper(Console.ReadKey().KeyChar);
+            if (input == 'A')
+                AddOutfitMenu.Print(userId);
+            else
+                MainMenu.Print();
+        }
+
+        private static void PrintOutfitList(int userId)
+        {
+            var outfits = OutfitService.GetOutfitsByUserId(userId);
+
+            foreach (var outfit in outfits)
+            {
+                var date = (outfit.Date).ToString("dd.MM.yyyy");
+                Console.WriteLine($"{outfit.Name,18} : {date}");
+            }
+        }
+    }
+}
